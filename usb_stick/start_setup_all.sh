05d5910a6cfaff40aab19b8e38f8386dd4504124
u206123 <florian.seidl@sbb.ch>
2016-06-22 23:06:22 +0200

    fixed more bugs...

--- usb_stick/start_setup_all.sh
+++ usb_stick/start_setup_all.sh
@@ -10,12 +10,14 @@ fi
 
 wget -q -O- http://www.search.ch >> /dev/null
 if [[ $? -ne 0 && -f $usbstick/setup_freesbb ]]; then
+    echo "Setup free sbb in order to establish network connection..."
     bash $usbstick/setup_freesbb.sh
     read -n1 -rsp $'Wait for the network connection to be established and press any key to continue if its OK or Ctrl+C to exit...\n'
 fi
 
 mkdir -p ~/cimon
 if [[ -f $usbstick/key.bin ]]; then
+    echo "Copying key.bin..."
     cp $usbstick/key.bin ~/cimon/key.bin
 fi
 
@@ -28,14 +30,18 @@ if [[ ! $CIMON_BRANCH ]]; then
 fi
 git clone https://github.com/SchweizerischeBundesbahnen/cimon_setup.git -b $CIMON_BRANCH
 
-cd /tmp/cimon_github/cimon_setup
-bash ./set_hostname.sh $1
+cd cimon_setup
+echo "Set hostname..."
+sudo bash ./set_hostname.sh $1 >> /dev/null
+echo "Setup..."
 bash ./setup.sh
 
 # free sbb update config
+echo "Setup update config..."
 bash ./setup_update_config.sh $2 $3
 # free sbb if not allready installed
 if [[ ! -d /opt/cimon/freesbb ]]; then
+    echo "Setup free sbb..."
     bash ./setup_freesbb.sh
 fi
 
