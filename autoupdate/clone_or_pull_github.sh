e1000da8c722e87cdd13e4cb2a7e9bf89b49596a
u206123 <florian.seidl@sbb.ch>
2016-07-20 21:59:58 +0200

    bug due to now calling it .git_branch

--- autoupdate/clone_or_pull_github.sh
+++ autoupdate/clone_or_pull_github.sh
@@ -2,7 +2,7 @@
 # Copyright (C) Schweizerische Bundesbahnen SBB, 2016
 WORKSPACE=$1
 REPO=$2
-if [[ -f ~/cimon/git_branch ]]; then
+if [[ -f ~/cimon/.git_branch ]]; then
     BRANCH=$(cat ~/cimon/.git_branch)
 else
     BRANCH="master"
