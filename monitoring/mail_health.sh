2f70b9485b98e17b46e91497480c0bdf10383c90
u206123 <florian.seidl@sbb.ch>
2020-02-12 23:26:28 +0100

    fix: use dist upgrade, improve mail script

--- monitoring/mail_health.sh
+++ monitoring/mail_health.sh
@@ -2,4 +2,22 @@
 # Copyright (C) Schweizerische Bundesbahnen SBB, 2016
 # mail the current state from the logs regularily
 
-echo -e "Current health state of cimon $(hostname) at $(date)\n\n-----cimon.log-----\n$(tail -99l /var/log/cimon/cimon.log)\n\n-----cimon_stdouterr.log-----\n$(tail -29l /var/log/cimon/cimon_stdouterr.log)\n\n-----update_config.log-----\n$(tail -29l /var/log/cimon/update_config.log)\n\n-----autoupdate_cimon.log-----\n$(tail -29l /var/log/cimon/autoupdate_cimon.log)\n\n-----mail_disk_full.log-----\n$(tail -19l /var/log/cimon/mail_disk_full.log)\n\n-----mail_address.log-----\n$(tail -19l /var/log/cimon/mail_address.log)" | mail -s "CIMON $(hostname): Health state info" $(cat ~/cimon/.mailto)
+function TailLog(){
+    local FILE=/var/log/cimon/$1
+    if [[ -f  $FILE ]]; then
+        echo $(tail -$2l $FILE)
+    else
+        echo "empty"
+    fi
+}
+
+TO=$(cat ~/cimon/.mailto)
+HOST=$(hostname)
+LOG=$(TailLog cimon.log 99)
+STDOUTLOG=$(TailLog cimon_stdouterr.log 29)
+CONFIGLOG=$(TailLog update_config.log 29)
+AUTOUPDATELOG=$(TailLog autoupdate_cimon.log 29)
+DISKFULLLOG=$(TailLog mail_disk_full.log 19)
+MAILADRESSLOG=$(TailLog mail_address.log 19)
+
+echo -e "Current health state of cimon $HOST at $(date)\n\n-----cimon.log-----\n$LOG\n\n-----cimon_stdouterr.log-----\n$STDOUTLOG\n\n-----update_config.log-----\n$CONFIGLOG\n\n-----autoupdate_cimon.log-----\n$AUTOUPDATELOG\n\n-----mail_disk_full.log-----\n$DISKFULLLOG\n\n-----mail_address.log-----\n$MAILADRESSLOG" | mail -s "CIMON $HOST: Health state info" $TO
